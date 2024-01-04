acf82f9aee75c60384bc0e3b38ddfb903da57fc9
AbishegaBruce <155568830+AbishegaBruce@users.noreply.github.com>
2024-01-04 15:30:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <h1 className="welcome-heading">Welcome to My App</h1>
+     
       <ExampleComponent />
     </div>
   );
 };
 
 export default App;
+
+
